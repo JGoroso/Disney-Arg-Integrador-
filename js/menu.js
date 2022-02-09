7170e67c8c6e8fd557c333ba5ac16696a7eb1ee5
Joel Goroso <yoelgoroso@gmail.com>
2022-02-09 10:34:17 -0300

    Queries 4

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,18 @@
-let btnMenu = document.getElementById("btn-menu");
-let menu = document.getElementById("main-nav");
-btnMenu.addEventListener("click", function () {
-  menu.classList.toggle("mostrar");
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
+
+navToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
+
+body.addEventListener("click", () => {
+  navMenu.classList.remove("nav-menu_visible");
 });
